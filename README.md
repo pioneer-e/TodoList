eca301083b2a771cf9e7cf2a48bb205d0e900756
codingPpushu <codingPpushu@gmail.com>
2021-07-08 23:47:35 +0900

    Todo-Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Todo List
+
+## Skills
+
+- 💻 HTML, CSS, JavaScript
+
+## Examples of Work
+
+- 📝 Add to item
+- ❌ Remove to item
+- 🖱 Button hover animation
+
+<br/>
+<img src = "./main.jpg" width='80%'>
---
+++ main.js
@@ -0,0 +1,53 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+function onAdd() {
+  const text = input.value;
+
+  if (text === '') {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
+  input.value = '';
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    onAdd();
+  }
+});
