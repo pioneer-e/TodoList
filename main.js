d49767711555ddb3fcd83f59a4da2681bd991130
codingPpushu <codingPpushu@gmail.com>
2021-07-09 20:36:16 +0900

    JS: Refactoring

--- main.js
+++ main.js
@@ -16,29 +16,20 @@ function onAdd() {
   input.value = '';
 }
 
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+      <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete">
+          <i class="fas fa-times" data-id=${id}></i>
+        </button>
+      </div>
+  `;
+  id++;
   return itemRow;
 }
 
@@ -49,5 +40,14 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     onAdd();
+  }
+});
+
+items.addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
+
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
